23a21446a754685606e9c59ea13579d227eca8cd
Tom Brückner <tom@marblear.com>
2025-02-17 14:07:28 +0100

    updated http package to 3.0.0

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'meteoreact:accounts',
   summary: 'Simple and intuative accounts view layer with react',
-  version: '1.2.4_2',
+  version: '1.2.4_3',
   documentation: 'README.md',
   git: 'https://github.com/royGil/accounts-react'
-})
+});
 
 Package.onUse(api => {
-  api.versionsFrom(['1.6.1', '2.3', '3.0'])
+  api.versionsFrom(['1.6.1', '2.3', '3.0']);
 
   api.use([
     'ecmascript',
@@ -15,15 +15,15 @@ Package.onUse(api => {
     'accounts-password',
     'mdg:validated-method@1.3.0',
     'check'
-  ], ['client', 'server'])
+  ], ['client', 'server']);
 
-  api.use('react-meteor-data', 'client')
+  api.use('react-meteor-data', 'client');
 
-  api.use('service-configuration', { weak: true })
-  api.use('http', 'server')
+  api.use('service-configuration', { weak: true });
+  api.use('http@3.0.0', 'server');
 
-  api.mainModule('index.js', ['client', 'server'])
-})
+  api.mainModule('index.js', ['client', 'server']);
+});
 
 Package.onTest(api => {
   api.use([
@@ -35,10 +35,10 @@ Package.onTest(api => {
     'mdg:validated-method@1.1.0',
     'react-meteor-data@0.2.16',
     'cultofcoders:mocha'
-  ])
+  ]);
 
-  api.use('http', 'server')
+  api.use('http@3.0.0', 'server');
 
-  api.mainModule('__tests__/client.test.js', 'client')
-  api.mainModule('__tests__/server.test.js', 'server')
-})
+  api.mainModule('__tests__/client.test.js', 'client');
+  api.mainModule('__tests__/server.test.js', 'server');
+});
